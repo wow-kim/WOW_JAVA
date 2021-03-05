43590f662bbc7b76b16e832c28d13112ec1607d6
kth10421@uos.ac.kr <kth10421@uos.ac.kr>
2021-03-05 12:47:57 +0900

    commit

---
+++ 객체지향프로그래밍및실습/1주차/src/hello/Intro.java
@@ -0,0 +1,23 @@
+package hello;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요?");
+		System.out.println("저는 자바를 처음 공부합니다.");
+		System.out.println("저의 나이는 25살입니다.");
+	}
+
+}
+
+// 한 파일 안에 퍼블릭 클래스는 한개, 그 퍼블릭 클래스와 파일 이름은 같아야함.
+// 파일을 옮길 때 꼭 import, export로 옮겨야합니다.
+
+class A {
+	
+}
+
+class B {
+	
+}
